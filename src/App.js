c88e02d53d7eb8ac76278667ab0f255f1bd14af8
Muhd Danial <mdanial1410@outlook.com>
2021-01-21 14:52:34 +0800

    added amplify signup and login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
+
+Amplify.configure(awsconfig);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Welcome!!!!</h1>
+			</header>
+			<body className="App-body">
+				<p>Add a file upload function here?</p>
+			</body>
+			<footer className="App-footer">
+				<AmplifySignOut />
+			</footer>
+		</div>
+	);
 }
 
-export default App;
+export default withAuthenticator(App);
